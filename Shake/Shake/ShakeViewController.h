75f00c67551bfb12da089071def1d1aaeeb30600
llbing <1083158667@qq.com>
2015-07-08 10:22:33 +0800

    add Shake

---
+++ Shake/Shake/ShakeViewController.h
@@ -0,0 +1,13 @@
+//
+//  ShakeViewController.h
+//  Shake
+//
+//  Created by bing on 15/5/20.
+//  Copyright (c) 2015年 bing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ShakeViewController : UIViewController
+
+@end
---
+++ Shake/Shake/ShakeViewController.m
@@ -0,0 +1,51 @@
+//
+//  ShakeViewController.m
+//  Shake
+//
+//  Created by bing on 15/5/20.
+//  Copyright (c) 2015年 bing. All rights reserved.
+//
+
+#import "ShakeViewController.h"
+
+@interface ShakeViewController ()
+
+@end
+
+@implementation ShakeViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (void)motionBegan:(UIEventSubtype)motion withEvent:(UIEvent *)event
+{
+    if (event.type == UIEventSubtypeMotionShake)
+    {
+        NSLog(@"Shake Began");
+    }
+}
+
+-(void)motionEnded:(UIEventSubtype)motion withEvent:(UIEvent *)event
+{
+    if (event.type == UIEventSubtypeMotionShake)
+    {
+        NSLog(@"Shake End");
+    }
+}
+
+-(void)motionCancelled:(UIEventSubtype)motion withEvent:(UIEvent *)event
+{
+    if (event.type == UIEventSubtypeMotionShake)
+    {
+        NSLog(@"Shake Cancelled");
+    }
+}
+
+@end
